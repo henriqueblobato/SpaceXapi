b3e9c5b5a370387e3fbbbfc337362f40db31b1f3
henriquelobato <iklobato1@gmail.com>
2022-03-31 03:14:18 -0300

    default db

--- spaceXapi/settings.py
+++ spaceXapi/settings.py
@@ -94,13 +94,17 @@ WSGI_APPLICATION = 'spaceXapi.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'postg': {
         'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
