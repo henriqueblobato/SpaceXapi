9c0275538d633530d0798d47270fa20dcbc6bcec
henriquelobato <iklobato1@gmail.com>
2022-03-31 02:53:52 -0300

    spacetrack fix post create

--- api_space/serializers.py
+++ api_space/serializers.py
@@ -1,6 +1,8 @@
+from typing import OrderedDict
+
 from rest_framework import serializers
 
-from api_space.models import SpaceTrack, Launch, ObjectType
+from api_space.models import SpaceTrack, Launch, ObjectType, Country
 
 
 class LaunchSerializer(serializers.ModelSerializer):
@@ -23,6 +25,23 @@ class SpaceTrackSerializer(serializers.ModelSerializer):
     country_code = serializers.StringRelatedField(source='country_code.code')
     object_type = ObjectTypeSerializer()
     launch = LaunchSerializer()
+
+    def create(self, validated_data):
+        obj_type = dict(validated_data['object_type']).get('type')
+        if obj_type.isdigit():
+            obj_id = obj_type
+            object_type = ObjectType.objects.get(pk=obj_id)
+            validated_data['object_type'] = object_type
+        if isinstance(obj_type, str):
+            object_type = ObjectType.objects.get(type=obj_type)
+            validated_data['object_type'] = object_type
+
+        if isinstance(validated_data['launch'], OrderedDict):
+            launch_id = dict(validated_data['launch']).get('launch_id')
+            launch, _ = Launch.objects.get_or_create(launch_id=launch_id)
+            validated_data['launch'] = launch
+
+        return SpaceTrack.objects.create(**validated_data)
 
     def validate(self, attrs):
         return super().validate(attrs)
