350a53671151cfee17b58e3c81fcada023e25339
henriquelobato <iklobato1@gmail.com>
2022-03-31 03:27:38 -0300

    register admin

--- api_space/admin.py
+++ api_space/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from api_space.models import *
 
 # Register your models here.
+admin.register(Country)
+admin.register(ObjectType)
+admin.register(Launch)
+admin.register(SpaceTrack)
+
+admin.site.register(Done)
